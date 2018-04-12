3502862a81cacb9dfeb448ace05d2a6a3cf8ea4a
Rafael Staib <me@rafaelstaib.com>
2018-04-12 17:19:36 +0200

    Written tests for Column and Row #11

--- src/row/Row.test.tsx
+++ src/row/Row.test.tsx
@@ -6,39 +6,31 @@ import { configure, shallow } from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
 import styled from "styled-components";
-import Container from "./Container";
+import Row from "./Row";
 
 configure({ adapter: new Adapter() });
 
-describe("<Container />", () => {
+describe("<Row />", () => {
   it("should match the snapshot (no properties set)", () => {
     // act
-    const result = shallow(<Container />);
+    const result = shallow(<Row />);
 
     // assert
     expect(result).toMatchSnapshot();
   });
 
-  it("should match the snapshot (fluid property set to true) ", () => {
+  it("should match the snapshot (noGutter property set to true) ", () => {
     // act
-    const result = shallow(<Container fluid />);
+    const result = shallow(<Row noGutter />);
 
     // assert
     expect(result).toMatchSnapshot();
   });
 
-  it("should match the snapshot (width property set to { xs: 100, sm: 200, md: 300 })", () => {
-    // act
-    const result = shallow(<Container width={{ xs: 100, sm: 200, md: 300 }} />);
-
-    // assert
-    expect(result).toMatchSnapshot();
-  });
-
-  it("should match the snapshot (ignores width when fluid is set)", () => {
+  it("should match the snapshot (width property set to { xs: true, sm: false, md: true })", () => {
     // act
     const result = shallow(
-      <Container fluid={true} width={{ xs: 100, sm: 200, md: 300 }} />
+      <Row noGutter={{ xs: true, sm: false, md: true }} />
     );
 
     // assert
