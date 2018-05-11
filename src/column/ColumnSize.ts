daea63f423e9135b3d0078ee2da727c5b61fd028
Rafael Staib <me@rafaelstaib.com>
2018-05-11 17:00:17 +0200

    Improved code reuse (#30)

--- src/column/ColumnSize.ts
+++ src/column/ColumnSize.ts
@@ -1,17 +1,5 @@
-type ColumnSize =
-  | "auto"
-  | "none"
-  | 1
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 12;
+import Columns from "./Columns";
+
+type ColumnSize = "auto" | "none" | Columns;
 
 export default ColumnSize;
