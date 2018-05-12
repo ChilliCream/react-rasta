493d6cb4947856667376320a889dee7001ce2b0f
Rafael Staib <me@rafaelstaib.com>
2018-05-12 19:09:05 +0200

    Set tslint rule 	railing-comma esSpecCompliant

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -4,8 +4,6 @@ import flatten from "./flatten";
 
 export default (
   strings: TemplateStringsArray,
-  // note: work-a-round -> tslint is expecting trailing comma here but this is a bug
-  // tslint:disable-next-line:trailing-comma
   ...interpolations: SimpleInterpolation[]
 ): string => {
   return flatten(css(strings, ...interpolations));
