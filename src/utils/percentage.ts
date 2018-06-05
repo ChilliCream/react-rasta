c710e1647f5f762e4bd79873799a5b52e4fde038
Rafael Staib <me@rafaelstaib.com>
2018-06-05 20:41:40 +0200

    Fixed some issues

--- src/utils/percentage.ts
+++ src/utils/percentage.ts
@@ -1,6 +1,6 @@
 export default function renderOrder(size: number): string {
   if (size != null && size > 0 && size < 13) {
-    return ((size / 12) * 100).toFixed(6);
+    return (size / 12 * 100).toFixed(6);
   }
 
   return "0";
